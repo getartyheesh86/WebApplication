4bdd088bba25b8650a98d50345a9f33be3844a41
Ratheesh Kumar <ratheesh.ar@tcs.com>
2019-05-11 22:25:59 +0530

    first commit

---
+++ WebApplication.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Controllers;
+using WebApplication.Models;
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void Register()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Register() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Login()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Login(string.Empty) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+
+}
